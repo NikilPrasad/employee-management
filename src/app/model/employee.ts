6fc109b2fefaa07f4bd2ee5bfbd57e051025c35b
Nikil Prasad <nikilram99@gmail.com>
2022-05-11 14:29:02 +0530

    Initial Commit

---
+++ src/app/model/employee.ts
@@ -0,0 +1,10 @@
+export class Employee {
+    id : number=0;
+    name : string='';
+    email : string='';
+    salary :number =0;
+
+    // constructor(public id:number,  public name:string, public email:string, public salary:number){}
+
+    
+}
---
+++ src/app/service/employee.service.ts
@@ -0,0 +1,71 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Observable, Observer } from 'rxjs';
+import { Employee } from '../model/employee';
+import { EmbeddedTemplateAst } from '@angular/compiler';
+
+
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeeService {
+
+  addEmpURL : string;
+  getEmpURL : string;
+  updateEmpUrl : string;
+  deleteEmpUrl : string;
+
+  constructor(private http : HttpClient) {
+
+    this.addEmpURL = 'http://localhost:9091/emp/addEmployee';
+    this.getEmpURL = 'http://localhost:9091/emp/getAll';
+    this.updateEmpUrl = 'http://localhost:9091/emp/updateEmployee';
+    this.deleteEmpUrl = 'http://localhost:9091/emp/deleteEmployeeById';
+
+   }
+
+
+   employees = [
+     {id:1,name:'amal',email:'amal@tcs.com',salary:10000},
+     {id:2,name:'ajay',email:'ajay@tcs.com',salary:20000},
+     {id:3,name:'akash',email:'akash@tcs.com',salary:5000},
+     {id:4,name:'naveen',email:'naveen@tcs.com',salary:15000},
+     {id:5,name:'kishor',email:'kishor@tcs.com',salary:30000},
+     {id:6,name:'abi',email:'abi@tcs.com',salary:10000},
+     {id:7,name:'evan',email:'evan@tcs.com',salary:10000}  
+    
+   ]
+
+
+
+   addEmployee(emp : Employee): Observable<Employee> {
+     return this.http.post<Employee>(this.addEmpURL,emp);
+   }
+
+   getAllEmployee(): Observable<Employee[]>{
+    return new Observable((response: Observer<Employee[]>)=>{
+      response.next(this.employees);
+      response.complete();
+      return ()=>{};
+
+    })
+   }
+
+   updateEmployee(emp :Employee) : Observable<Employee>{
+     return this.http.put<Employee>(this.updateEmpUrl, emp);
+   }
+
+   deleteEmployee(emp : Employee) : Observable<Employee> {
+    return new Observable((response: Observer<Employee>)=>{
+      
+      response.next(this.employees[0]);
+      response.complete();
+      return ()=>{};
+
+    })
+   }
+  
+
+}
